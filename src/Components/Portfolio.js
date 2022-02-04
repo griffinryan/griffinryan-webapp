f6823cff6b40de55b567a85b380c271f6f12cfbd
Griffin Ryan <griffinryan@Griffins-MacBook-Pro.local>
2022-02-03 18:03:47 -0800

    Added resume and research papers to portfolio.

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,7 +30,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Check Out Some Works by Torpoise.</h1>
+            <h1>Check Out Some Works by Griffin Ryan.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
